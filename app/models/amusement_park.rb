507e493229c59698fb4fd4d6d62eac86efff33de
Nick King <nickmaxking@gmail.com>
2020-10-23 10:43:05 -0600

    Add average_thrill rating model method

--- app/models/amusement_park.rb
+++ app/models/amusement_park.rb
@@ -2,4 +2,8 @@ class AmusementPark < ApplicationRecord
   has_many :rides
 
   validates_presence_of :name, :admission_price
+
+  def average_thrill_rating
+    rides.average(:thrill_rating).to_f
+  end
 end
